f503aa81c55c208d0dd52df061d66a49628b97ae
Jacob Molsby <jacobmolsby@hotmail.se>
2022-01-16 07:29:56 +0000

    Landing page complete

--- main/views.py
+++ main/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+
+def index(request):
+    """
+    A view to display home page.
+    """
+
+    return render(request, 'main/index.html')
