6799b204c6c3474733ec88d8e4ef4a0d3a012dcc
Jacob Molsby <jacobmolsby@hotmail.se>
2022-01-24 11:21:08 +0000

    Added Domain to allowed hosts

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -25,14 +25,18 @@ SECRET_KEY = os.environ.get('SECRET_KEY', '')
 DEBUG = 'DEVELOPMENT' in os.environ
 
 ALLOWED_HOSTS = [
-    'localhost',
+    'makeupbymelanierider.com',
+    'www.makeupbymelanierieder.com',
     'melanierieder.herokuapp.com',
+    'localhost',
 ]
 
 CSRF_TRUSTED_ORIGINS = [
+    'https://makeupbymelanierieder.com',
+    'https://www.makeupbymelanierider.com',
+    'https://www.melanierieder.herokuapp.com',
     'https://8000-jacobmolsby93-melaniepor-t40xp862yg1.ws-us27.gitpod.io',
     'https://8000-jacobmolsby93-melaniepor-t40xp862yg1.ws-eu27.gitpod.io',
-    'https://www.melanierieder.herokuapp.com',
 ]
 
 CORS_ALLOW_CREDENTIALS = True
